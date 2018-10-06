7b2debb039111dbc2028a765265f6ea73d1e97e3
Jacob Grimm <jacob.d.grimm@gmail.com>
2018-10-06 18:44:59 -0400

    adds a number choice and an int array

---
+++ GuessMyNumber/BasicNumber.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessMyNumber
+{
+    class BasicNumber
+    {
+        int[] num = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        public void NumberPicker()
+        {
+            Console.WriteLine("What number do you want?");
+            int chosenNumber = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
