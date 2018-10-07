fed7d44c4d581a5c627b754b9597c6c74cbad27c
Jacob Grimm <jacob.d.grimm@gmail.com>
2018-10-06 23:09:12 -0400

    adds the human guessing game method

---
+++ GuessMyNumber/ComputerPlays.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessMyNumber
+{
+    class ComputerPlays
+    {
+        int chosenNumber;
+
+        public void NumberPicker()
+        {
+            Console.WriteLine("What do you want me to guess?");
+            try
+            {
+                int pickedNumber = Convert.ToInt32(Console.ReadLine());
+                if (pickedNumber > 10 || pickedNumber < 1)
+                {
+                    Console.WriteLine("Number out of bounds! Try again");
+                    NumberPicker();
+                }
+                chosenNumber = pickedNumber;
+            }
+            catch
+            {
+                Console.WriteLine("invalid entry");
+                NumberPicker();
+
+            }
+        }
+    }
+}
--- GuessMyNumber/HumanPlays.cs
+++ GuessMyNumber/HumanPlays.cs
@@ -8,6 +8,50 @@ namespace GuessMyNumber
 {
     class HumanPlays
     {
+        Random rnd = new Random();
 
+        int numberToGuess;
+
+        int guessCount = 1;
+
+        public void NumberPicker()
+        {
+            numberToGuess = rnd.Next(1, 1001);
+        }
+
+        public void GuessingGame()
+        {
+            Console.WriteLine("What's my number, human?");
+            try
+            {
+                int humanGuess = Convert.ToInt32(Console.ReadLine());
+                if (humanGuess > 1000 || humanGuess < 1)
+                {
+                    Console.WriteLine("That number is out of the limits of this game");
+                    GuessingGame();
+                }
+                if (humanGuess == numberToGuess)
+                {
+                    Console.WriteLine($"Good job, human, you guessed my number in {guessCount} guesses!");
+                    return;
+                }
+                if (humanGuess > numberToGuess)
+                {
+                    Console.WriteLine("You guessed too high, humman!");
+                    guessCount++;
+                    GuessingGame();
+                }
+                if (humanGuess < numberToGuess)
+                {
+                    Console.WriteLine("You guessed too low, human!");
+                    guessCount++;
+                    GuessingGame();
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Nice try, human. That isn't a number...");
+            }
+        }
     }
 }
