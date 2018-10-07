b0d98e4763c852aefbef5be6f509032c7427a959
Jacob Grimm <jacob.d.grimm@gmail.com>
2018-10-06 20:56:31 -0400

    makes a method that detects if the number is high or low

--- GuessMyNumber/BasicNumber.cs
+++ GuessMyNumber/BasicNumber.cs
@@ -9,11 +9,31 @@ namespace GuessMyNumber
     class BasicNumber
     {
         int[] num = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int chosenNumber;
+        
 
         public void NumberPicker()
         {
             Console.WriteLine("What number do you want?");
-            int chosenNumber = Convert.ToInt32(Console.ReadLine());
+            int pickedNumber = Convert.ToInt32(Console.ReadLine());
+            chosenNumber = pickedNumber;
+        }
+
+
+        public void Bisector()
+        {
+            if (chosenNumber == (num.Length / 2) )
+            {
+                Console.WriteLine($"The number has been found! {chosenNumber} was the number");
+            }
+            if (chosenNumber > (num.Length / 2))
+            {
+                Console.WriteLine($"{ num.Length / 2} is too low!");
+            }
+            if (chosenNumber < (num.Length/ 2))
+            {
+                Console.WriteLine($"{num.Length / 2} is too high!");
+            }
         }
     }
 }
