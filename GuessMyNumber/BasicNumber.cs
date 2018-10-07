fdd9eb992200da20d0d9ade5e3f2a8e6471122a8
Jacob Grimm <jacob.d.grimm@gmail.com>
2018-10-06 22:28:21 -0400

    adds error handling

--- GuessMyNumber/BasicNumber.cs
+++ GuessMyNumber/BasicNumber.cs
@@ -15,8 +15,22 @@ namespace GuessMyNumber
         public void NumberPicker()
         {
             Console.WriteLine("What number do you want?");
-            int pickedNumber = Convert.ToInt32(Console.ReadLine());
-            chosenNumber = pickedNumber;
+            try
+            {
+                int pickedNumber = Convert.ToInt32(Console.ReadLine());
+                if (pickedNumber > 10 || pickedNumber < 1)
+                {
+                    Console.WriteLine("Number out of bounds! Try again");
+                    NumberPicker();
+                }
+                chosenNumber = pickedNumber;
+            }
+            catch
+            {
+                Console.WriteLine("invalid entry");
+                NumberPicker();
+
+            }
         }
 
 
