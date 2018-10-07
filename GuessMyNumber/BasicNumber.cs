8e7358f1124467e64125574c7c92e4faa53375d6
Jacob Grimm <jacob.d.grimm@gmail.com>
2018-10-06 21:36:40 -0400

    adds  a recursive system to find the chosen number

--- GuessMyNumber/BasicNumber.cs
+++ GuessMyNumber/BasicNumber.cs
@@ -8,7 +8,7 @@ namespace GuessMyNumber
 {
     class BasicNumber
     {
-        int[] num = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
         int chosenNumber;
         
 
@@ -20,7 +20,7 @@ namespace GuessMyNumber
         }
 
 
-        public void Bisector()
+        public void Bisector(int[] num)
         {
             if (chosenNumber == (num.Length / 2) )
             {
@@ -29,10 +29,22 @@ namespace GuessMyNumber
             if (chosenNumber > (num.Length / 2))
             {
                 Console.WriteLine($"{ num.Length / 2} is too low!");
+                int[] newr = new int[(num.Length / 2) - 1];
+                for (int i = 0; i < newr.Length; i++)
+                {
+                    newr[i] = num[i + num.Length / 2];
+                }
+                Bisector(newr);
             }
             if (chosenNumber < (num.Length/ 2))
             {
                 Console.WriteLine($"{num.Length / 2} is too high!");
+                int[] newr = new int[(num.Length / 2) - 1];
+                for (int i = 0; i < newr.Length; i++)
+                {
+                    newr[i] = num[i];
+                }
+                Bisector(newr);
             }
         }
     }
